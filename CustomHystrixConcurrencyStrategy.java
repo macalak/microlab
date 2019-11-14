a6a95406896e7482808bad0ca38f720aea6d17ce
Ivan Macalák <macalak@itexperts.sk>
2019-11-14 16:37:29 +0100

    Add files via upload

---
+++ CustomHystrixConcurrencyStrategy.java
@@ -0,0 +1,50 @@
+package sk.posam.sp.devicegtw.application.interceptor;
+
+import java.util.Map;
+import java.util.concurrent.Callable;
+
+import org.springframework.stereotype.Component;
+
+import com.netflix.hystrix.strategy.HystrixPlugins;
+import com.netflix.hystrix.strategy.concurrency.HystrixConcurrencyStrategy;
+
+import sk.posam.sp.devicegtw.application.util.ThreadContext;
+
+
+@Component
+public class CustomHystrixConcurrencyStrategy 
+	extends HystrixConcurrencyStrategy {
+
+    public CustomHystrixConcurrencyStrategy() {
+        
+    	HystrixPlugins.reset();
+        HystrixPlugins.getInstance().registerConcurrencyStrategy(this);
+    }
+
+    @Override
+    public <T> Callable<T> wrapCallable(Callable<T> callable) {
+        return new HystrixContextWrapper<T>(callable);
+    }
+
+    public static class HystrixContextWrapper<V> implements Callable<V> {
+    	Map<String, Object> threadData;
+        private Callable<V> delegate;
+
+        public HystrixContextWrapper(Callable<V> delegate) {
+        	this.threadData = ThreadContext.getThreadData();
+            this.delegate = delegate;
+        }
+
+        @Override
+        public V call() throws Exception {
+        	Map<String, Object> existingStateData = ThreadContext.getThreadData();
+
+            try {
+                ThreadContext.setThreadData(this.threadData);
+                return this.delegate.call();
+            } finally {
+            	ThreadContext.setThreadData(existingStateData);
+            }
+        }
+    }
+}
